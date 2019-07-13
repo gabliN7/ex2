6f90bc8dcdb0f681f1df98a6c3e583308c656989
Thanayut Suwannasin <59143249@g.cmru.ac.th>
2019-07-13 14:13:39 +0700

    เพิ่มหน้าtest

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const port = 3000
 app.get('/', (req, res) => res.send('gabliN'))
 app.get('/about', (req, res) => res.send('เกี่ยวกับเรา'))
 app.get('/contact', (req, res) => res.send('ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
